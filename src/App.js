10407f68df4ba7bf887ec020dedd497f49219e75
shettyrohan183 <76105128+shettyrohan183@users.noreply.github.com>
2022-02-19 10:36:36 +0530

    router Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is r Notebook</h1>
-    </div>
+   <>
+   <Router>
+   <Navbar/>
+    <Routes>
+    <Route exact path="/" element={<Home />}/>
+    <Route exact path="/about" element={<About />}/>
+    </Routes>
+    </Router>
+      </>
   );
 }
 
