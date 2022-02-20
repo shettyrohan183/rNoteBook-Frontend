11f17645727b50f420fe056d0180aabe662f6143
shettyrohan183 <76105128+shettyrohan183@users.noreply.github.com>
2022-02-20 12:34:16 +0530

    fetching data

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,17 +1,14 @@
 
 import React , { useContext,useEffect } from 'react';
-import noteContext from '../context/notes/noteContext';
+
 
 
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(()=>{
-      a.update();
-       // eslint-disable-next-line
-  },[])
+  
+ 
   return (
     <div>
-      This is about us {a.state.name} and he is in {a.state.class}
+      This is about us
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,38 @@ import NoteContext from "./noteContext"
 import { useState } from "react"
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name" :"Harry",
-        "class":"5b"
-    }
-    const [state, setState] = useState(s1)
-    const update = ()=>{
-        setTimeout(()=>{
-            setState({
-                "name":"Larry",
-                "class":"10b"
-            })
-        },1000);
-    }
-    return (
-        <NoteContext.Provider value = {{state:state,update:update}}>
+    const noteinitial = [
+        {
+          "_id": "620ca17a7a8aab9276fd2d6f",
+          "user": "620c7b47af6d28c6274944d3",
+          "title": "hhhhmytitle",
+          "description": "this is my first note",
+          "tag": "firstone",
+          "date": "2022-02-16T07:02:18.421Z",
+          "__v": 0
+        },
+        {
+          "_id": "620ca17b7a8aab9276fd2d71",
+          "user": "620c7b47af6d28c6274944d3",
+          "title": "hhhhmytitle",
+          "description": "this is my first note",
+          "tag": "firstone",
+          "date": "2022-02-16T07:02:19.565Z",
+          "__v": 0
+        },
+        {
+          "_id": "620ca17c7a8aab9276fd2d73",
+          "user": "620c7b47af6d28c6274944d3",
+          "title": "hhhhmytitle",
+          "description": "this is my first note",
+          "tag": "firstone",
+          "date": "2022-02-16T07:02:20.492Z",
+          "__v": 0
+        }
+      ]
+      const [notes, setnotes] = useState(noteinitial)
+          return (
+        <NoteContext.Provider value = {{notes, setnotes}}>
             {props.children}
         </NoteContext.Provider>
     )
